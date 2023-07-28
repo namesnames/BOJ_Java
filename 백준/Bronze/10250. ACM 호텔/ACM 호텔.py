70dd65979268c033bc2203c6a07b998398519ce9
namesnames <103047410+namesnames@users.noreply.github.com>
2023-07-28 18:23:00 +0900

    [Bronze III] Title: ACM 호텔, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -3,7 +3,7 @@ for i in range(T):
     a,b,c = map(int,input().split())
     floor = 0
     room = 0
-    if((a == c) or (c%a==0)):
+    if((a == c) or (c%a==0)):     #나머지가 0이 되는 순간을 조심해야함.. 그리고 1 5 4 같은 반례도 주의
         floor = a
         room = c // a
     else:
