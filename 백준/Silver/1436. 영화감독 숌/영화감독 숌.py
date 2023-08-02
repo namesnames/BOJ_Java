b37b428dd24affc3947405ac7b0e71e6973c3c81
namesnames <103047410+namesnames@users.noreply.github.com>
2023-08-02 21:29:25 +0900

    [Silver V] Title: 영화감독 숌, Time: 588 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,12 @@
+candidate = []
+i = 666
+cnt = 0
+n = int(input())
+while True:
+    if("666" in str(i)):
+        cnt += 1
+        if(cnt == n):
+            print(i)
+            break
+    i += 1
+
