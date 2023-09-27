8c7ccf7a688103cc9885aa499f6138ede7bfce8e
namesnames <103047410+namesnames@users.noreply.github.com>
2023-09-27 21:51:58 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,13 @@
+N,M = map(int,input().split())
+
+s = []
+
+def backtracking(start):
+    if(len(s) == M):
+        print(' '.join(map(str,s)))
+        return
+    for i in range(start,N+1):
+        s.append(i)
+        backtracking(i)
+        s.pop()
+backtracking(1)
