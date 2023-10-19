6ea02f98800f2c8a34ed41395b8b81933ec98f79
namesnames <103047410+namesnames@users.noreply.github.com>
2023-10-19 19:02:03 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,12 +1,12 @@
 word = input().upper()
-wordListSet = list(set(word)) #list로 안감쌌음
+wordList = list(set(word))
 
 cnt = []
-for i in range(len(wordListSet)):  # range 빼먹음
-    cnt.append(word.count(wordListSet[i]))
-cntMax = max(cnt)
+for i in range(len(wordList)):
+    cnt.append(word.count(wordList[i]))
+max = max(cnt)
 
-if cnt.count(cntMax) != 1:
-    print("?")
+if cnt.count(max) != 1:
+    print('?')
 else:
-    print(wordListSet[cnt.index(cntMax)]) # cntMax라는 값이 cnt의 몇 번째 인덱스에 있을까? cnt.index(cntMax)로 알 수 있음
+    print(wordList[cnt.index(max)])
