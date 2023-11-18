8386e124a2689e150e363528ac877c65386e80a3
namesnames <103047410+namesnames@users.noreply.github.com>
2023-11-19 01:55:57 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,7 +1,19 @@
-import itertools
-a,b = map(int,input().split())
-arr = [i for i in range(1,a+1)]
-li = list(itertools.combinations(arr,b))
-for a in sorted(li):
-    print(*a)
+N,M = map(int,input().split())
+
+s = []
+ans = []
+
+def backtracking(start):
+    if(len(s) == M):
+        ans.append(s[:])
+        return
     
+    for i in range(start,N+1):
+        if(i not in s):
+            s.append(i)
+            backtracking(i+1)
+            s.pop()
+
+backtracking(1)
+for a in ans:
+    print(*a)
