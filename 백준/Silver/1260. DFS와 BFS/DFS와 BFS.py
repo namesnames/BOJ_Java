7b2b38d910fae784e442bdaee811dbe27f739628
namesnames <103047410+namesnames@users.noreply.github.com>
2023-11-19 11:57:03 +0900

    [Silver II] Title: DFS와 BFS, Time: 568 ms, Memory: 40332 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,44 +1,39 @@
-from collections import deque
 
+from collections import deque
 N,M,V = map(int,input().split())
 
 def dfs(idx):
-    #global visted # visted를 가져와서 계속 써야하니 global
-    visted[idx] = 1 
-    print(idx,end = ' ')
+    global visted
+    visted[idx] = 1
+    print(idx,end= " ")
     for next in range(N+1):
-        if(visted[next] == 0 and matrix[idx][next] == 1):
-            dfs(next) 
-
+        if(visted[next] == 0 and matrix[idx][next] == 1 ):
+            dfs(next)
+          
+def bfs(V):
+    global dq
+    global visted
+    dq.append(V)
+    visted[V] = 1 
     
-def bfs():
     while dq:
         cur = dq.popleft()
-        print(cur,end = ' ')
+        print(cur,end = " ")
         for next in range(N+1):
             if(visted[next] == 0 and matrix[cur][next] == 1):
                 dq.append(next)
                 visted[next] = 1
-                    
-    
-# N+1 크기의 graph를 만들자
-matrix = [[0 for _ in range(N+1)] for _ in range(N+1)]
-visted = [0 for _ in range(N+1)]
+                
+matrix = [[0 for _ in range(N+1)] for _ in range(N+1)]                
 
 for _ in range(M):
     a,b = map(int,input().split())
     matrix[a][b] = matrix[b][a] = 1
     
-# dfs
-dfs(V)
-print()
-
 visted = [0 for _ in range(N+1)]
 
-# bfs
+dfs(V)
+print()
 dq = deque()
-dq.append(V)
-visted[V] = 1
-bfs()
-
-
+visted = [0 for _ in range(N+1)]
+bfs(V)    
