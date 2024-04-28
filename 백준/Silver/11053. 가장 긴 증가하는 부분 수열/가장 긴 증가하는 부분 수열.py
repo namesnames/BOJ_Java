306a0487ad6a4a046a84e3fb26675435818c42a7
namesnames <103047410+namesnames@users.noreply.github.com>
2024-04-28 16:22:42 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 104 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -6,9 +6,9 @@ arr = list(map(int,input().split()))
 # 나보다 전의 녀석들을 다 확인해봐야함
 # 나보다 작으면서 가지고 있는 값(그 녀석까지의 부분수열)이 큰 녀석을 pick
 
-dp = [1] * N
+dp = [1] * N # 우선 다 1로 초기화 -> 언제든 본인이 부분수열의 시작이 될 수 있기 때문
 for i in range(1,N):
-    tempDpMax = 1
+    tempDpMax = 1 #맨 loop마다 temp를 1로 초기화 -> 그래야 최대값이 올 때 tempDpMax를 그 최대값으로 바꿀 수 있기 때문
     for j in range(i-1,-1,-1):
         if(arr[i] > arr[j] and tempDpMax <= dp[j]):
             tempDpMax = dp[j]
