10424600dd1d87b8e0893da8f8dd133a3ebe87b2
namesnames <103047410+namesnames@users.noreply.github.com>
2024-05-07 00:37:49 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 320 ms, Memory: 64924 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,29 +1,28 @@
+
+# 2차원 배열에 간선정보 저장 (연결리스트 방식으로)
+# dfs(1)부터 시작
+# dfs(1)은 1에 연결된 정점들인 4와6의 부모를 1로 설정 (parent배열에 저장)
+# dfs(4)와 dfs(6)도 똑같이 실행
+# dfs(4)는 4에 연결된 정점들의 부모를 4로 설정
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-sys.setrecursionlimit(10**8) #재귀 리미트
 
-n = int(input())
-
-# 그래프 만들기
-graph = [[] for _ in range(n+1)]
-for _ in range(n-1):  # 간선수는 n-1개
-    x, y = map(int, input().split())  # [[1,2], [2,3] .....]
-    graph[x].append(y)
-    graph[y].append(x)
-visited = [0] * (n+1) #방문여부 대신, 부모를 담을 변수
-
-
-# dfs 함수
+N = int(input())
+matrix = [[] for _ in range(N+1)]
+for _ in range(N-1):
+    a,b = map(int,input().rstrip().split())
+    matrix[a].append(b)
+    matrix[b].append(a)
+    
+parent = [0] * (N+1)
 def dfs(v):
-   # 현재 V와 연결된 다른 노드를 재귀적으로 방문
-    for i in graph[v]:
-        if not visited[i]:  # 방문하지 않은 노드라면
-            # visited를 1이 아닌, 탐색을 시작한 값을 넣어준다 dfs는 항상 부모에서 자식으로 이동한다.
-            visited[i] = v
-            dfs(i)  # 해당 노드를 시작 노드로 dfs
+    for i in matrix[v]:
+        if(parent[i] == 0):
+            parent[i] = v
+            dfs(i)
+    return
 
-dfs(1) #항상 1이 시작노드
-
-# visited 출력
-for i in range(2, n+1):
-    print(visited[i])
+dfs(1)
+for i in range(2,N+1):
+    print(parent[i])
