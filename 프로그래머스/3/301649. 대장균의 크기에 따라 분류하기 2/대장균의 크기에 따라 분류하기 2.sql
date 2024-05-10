54db6236dd6a89b456d21ad3d09f0527aeb64a48
namesnames <103047410+namesnames@users.noreply.github.com>
2024-05-10 17:47:18 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,17 +1,41 @@
-with percentage as (
+# with percentage as (
+#     select id, 
+#             ntile(4) over(order by size_of_colony desc) as grade
+#     from ecoli_data
+# )
+
+# select e.id,
+#     case when p.grade = 1 then "CRITICAL"
+#         when p.grade = 2 then "HIGH"
+#         when p.grade = 3 then "MEDIUM"
+#         when p.grade = 4 then "LOW"
+#     end as COLONY_NAME
+# from ecoli_data e
+# inner join percentage p
+# on e.id = p.id
+# order by id
+with sub as(
     select id, 
-            ntile(4) over(order by size_of_colony desc) as grade
+    ntile(4) over (order by size_of_colony desc) as grade
     from ecoli_data
 )
 
-select e.id,
-    case when p.grade = 1 then "CRITICAL"
-        when p.grade = 2 then "HIGH"
-        when p.grade = 3 then "MEDIUM"
-        when p.grade = 4 then "LOW"
-    end as COLONY_NAME
+select e.id, 
+    case when sub.grade = 1 then "CRITICAL"
+         when sub.grade = 2 then "HIGH"
+         when sub.grade = 3 then "MEDIUM"
+         when sub.grade = 4 then "LOW"
+    end as colony_name
 from ecoli_data e
-inner join percentage p
-on e.id = p.id
-order by id 
-    
+join sub
+on e.id = sub.id
+order by e.id
+
+
+
+
+
+
+
+
+
