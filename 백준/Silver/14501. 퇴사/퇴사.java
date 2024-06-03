2fd538f5267ed9d626b45d9734781dee62ed369e
namesnames <103047410+namesnames@users.noreply.github.com>
2024-06-03 20:06:57 +0900

    [Silver III] Title: 퇴사, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,29 +1,36 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int max=0;
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		int Case = sc.nextInt();
-		int[] T = new int[Case];//소요 기간
-		int[] P = new int[Case];//금액
-		for(int i=0;i<Case;i++) {
-			T[i]=sc.nextInt();	
-			P[i]=sc.nextInt();	
-		}//for
-		
-		int[] dp = new int[Case+1];
-		
-		for(int i=0;i<Case;i++) {
-			 if(i+T[i]<=Case) {	//범위에 벗어나지 않는다면 
-				 dp[i+T[i]]=Math.max(dp[i+T[i]],dp[i]+P[i]);	
-			 }//if
-			 dp[i+1]=Math.max(dp[i+1],dp[i]);	//다음dp=현재 누적값vs 다음 누적값
-			 
-		}//for	
-		System.out.println(dp[Case]);	
-	}//main()
-	
-}//class Main
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] T = new int[N];
+        int[] P = new int[N];
+
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N+1];
+        for(int i=N-1; i>-1; i--) {
+            if (i + T[i] <= N) {
+                dp[i] = Math.max(dp[i + T[i]] + P[i], dp[i + 1]);
+            }
+            else{
+                dp[i] = dp[i+1];
+            }
+        }
+        
+        System.out.println(dp[0]);
+
+    }
+}
+
