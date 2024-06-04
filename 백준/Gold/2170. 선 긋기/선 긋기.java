2164c57d26f9ff15497bd0d3f01d98f96de454da
namesnames <103047410+namesnames@users.noreply.github.com>
2024-06-04 16:30:37 +0900

    [Gold V] Title: 선 긋기, Time: 1492 ms, Memory: 280848 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -2,7 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    // N이 1일때 분기처리는 그냥 if(N==1) 머시기 return으로 하자
+    // 주의할점 -> 1,5 2,4 와 같이 입력이 들어올 때를 대비 -> 다음녀석의 종료시간(4)이 end(5) 커야만 end를 갱신해야함
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -29,26 +31,27 @@ public class Main {
         int end = temp[1];
         long total = 0;
 
-        if(N==1){
+        if(N==1){  // 분기처리는 else쓰지말고
             System.out.println(end - start);
+            return; //이렇게 return하자
         }
-        else {
-            for (int i = 1; i < N; i++) {
-                int[] temp2 = list.get(i);
-                if (temp2[0] > end) { // 다음 녀석의 시작이 end보다 클 때 선길이 계산
-                    total += (end - start);
-                    start = temp2[0];
-                    end = temp2[1];
-                } else { // 다음녀석이 겹치거나 연결돼있으면 end만 수정
-                    if (temp2[1] > end) { // 그것도 다음녀석의 종료부분이 end보다 클 때만 1,5 2,4 입력 대비
-                        end = temp2[1];
-                    }
-                }
 
-                if (i == N - 1) total += (end - start); // 마지막이면 그냥 길이 계산
+        for (int i = 1; i < N; i++) {
+            int[] temp2 = list.get(i);
+            if (temp2[0] > end) { // 다음 녀석의 시작이 end보다 클 때 선길이 계산
+                total += (end - start);
+                start = temp2[0];
+                end = temp2[1];
+            } else { // 다음녀석이 겹치거나 연결돼있으면 end만 수정
+                if (temp2[1] > end) { // 그것도 다음녀석의 종료부분이 end보다 클 때만 1,5 2,4 입력 대비
+                    end = temp2[1];
+                }
             }
-            System.out.println(total);
+
+            if (i == N - 1) total += (end - start); // 마지막이면 그냥 길이 계산
         }
+        System.out.println(total);
+
     }
 }
 
