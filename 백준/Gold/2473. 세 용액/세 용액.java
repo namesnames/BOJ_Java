6bba985d273d09b212b166cce9eda3d7ce9c2e60
namesnames <103047410+namesnames@users.noreply.github.com>
2024-06-07 18:10:51 +0900

    [Gold III] Title: 세 용액, Time: 252 ms, Memory: 17272 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        long[] arr = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        long minA = Long.MAX_VALUE;
+        long minB = Long.MAX_VALUE;
+        long minC = Long.MAX_VALUE;
+        long min = Long.MAX_VALUE;
+        for(int i=0; i<N-2; i++){
+            int pin = i;
+            int start = i+1;
+            int end = N-1;
+
+            while(start < end){
+                long value = arr[pin] + arr[start] + arr[end];
+                if(Math.abs(min) > Math.abs(value)) {
+                    min = value;
+                    minA = arr[pin];
+                    minB = arr[start];
+                    minC = arr[end];
+                }
+
+                if(value == 0) {
+                    System.out.println(arr[pin] + " " + arr[start] + " " + arr[end]);
+                    System.out.println(" 설마");
+                    return;
+                }
+                else if(value > 0 ) end--; // 0보다 크면 오른쪽을 줄이고
+                else start ++;
+            }
+        }
+        System.out.println(minA + " " + minB + " " + minC);
+    }
+}
