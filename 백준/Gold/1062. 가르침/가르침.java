2419ded5db508774a7b64c663efdb8e6175c8287
namesnames <103047410+namesnames@users.noreply.github.com>
2024-06-08 20:15:53 +0900

    [Gold IV] Title: 가르침, Time: 360 ms, Memory: 16416 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,76 +1,70 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-	static int N,K,result = Integer.MIN_VALUE;
-	static boolean[] alphabet = new boolean[26];	//배운 알파벳 확인 배열
-	static ArrayList<String> word = new ArrayList<>();	//입력되는 단어 저장 리스트
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        	//입력값 처리하는 BufferedReader
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        	//결과값 출력하는 BufferedWriter
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		alphabetInit();	//'a', 'c', 'i', 'n', 't' 배웠다고 초기화
-		if(K<5)		//배울 수 있는 글자가 5개 이하일 때
-			bw.write("0");
-		else if(K==26)	//배울 수 있는 글자가 26개일 때
-			bw.write(N + "\n");
-		else {		//그 사이일 때
-        	//입력되는 남극 언어 저장
-			for(int i=0;i<N;i++) {
-				String temp = br.readLine();
-				temp.replace("anta", "");
-				temp.replace("tica", "");
-				word.add(temp);
-			}
-			cal(1,5);	//모든 경우의 수에서 읽을 수 있는 단어 최대 수 구하는 함수
-			bw.write(result + "\n");	//최대값 BufferedWriter 저장
-		}
-		bw.flush();		//결과 출력
-		bw.close();
-		br.close();
-	}
-    	//재귀로 알파벳을 배우는 모든 경우를 구하는 함수
-	static void cal(int alpha,int cur) {
-		if(cur==K) {
-			wordCheck();
-			return ;
-		}
-		for(int i=alpha;i<26;i++) {
-			if(alphabet[i])
-				continue;
-			alphabet[i] = true;
-			cal(i,cur+1);
-			alphabet[i] = false;
-		}
-		return;
-	}
-    	//배운 알파벳으로 단어를 몇 개 읽을 수 있는지 확인하는 함수
-	static void wordCheck() {
-		int count = 0;
-		for(int i=0;i<word.size();i++) {
-			String temp = word.get(i);
-			boolean ck = false;
-			if(!temp.equals("")) {
-				int size = temp.length();
-				for(int j=0;j<size;j++) {
-					if(!alphabet[temp.charAt(j)-97]) {
-						ck = true;
-						break;
-					}
-				}
-			}
-			if(!ck)
-				count++;
-		}
-		result = Math.max(result, count);
-		return;
-	}
-    	//'a', 'c', 'i', 'n', 't'가 배웠다고 초기화하는 함수
-	static void alphabetInit() {
-		alphabet[0] = alphabet[2] = alphabet[8] = alphabet[13] = alphabet[19] = true;
-		return;
-	}
+
+    static boolean[] alphabet = new boolean[26];
+    static int K;
+    static ArrayList<String> list = new ArrayList<>();
+    static int max = 0;
+
+    static int check(){
+        int cnt = 0;
+        for(int i = 0; i < list.size(); i++){
+            String temp = list.get(i);
+            boolean flag = true;
+            for(int j = 0; j < temp.length(); j++){
+                if(alphabet[temp.charAt(j) - 97] != true) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag == true) cnt++;
+        }
+        return cnt;
+    }
+
+    // 모든 알파벳들 중에서 K개를 뽑는 함수
+    static void cal(int start, int depth){
+        if(depth == K-5) {
+            max = Math.max(max,check());
+            return;
+        }
+        for(int i = start; i < 26; i++){
+            if(alphabet[i]) continue;
+            alphabet[i] = true;
+            cal(i+1, depth+1);
+            alphabet[i] = false;
+        }
+
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        alphabet[0] = alphabet[2] = alphabet[8] = alphabet[13] = alphabet[19] = true;
+
+        if(K < 5) {
+            System.out.println(0);
+            return;
+        }
+        else if(K == 26) {
+            System.out.println(N);
+            return;
+        }
+        else {
+            for (int i = 0; i < N; i++) {
+                String temp = br.readLine();
+                temp.replace("anta", "");
+                temp.replace("tica", "");
+                list.add(temp);
+            }
+            cal(1,0);
+
+        }
+        System.out.println(max);
+    }
 }
+
