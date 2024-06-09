d9def65b4fbee23850d2a6d8569ae20c4b6baaf5
namesnames <103047410+namesnames@users.noreply.github.com>
2024-06-09 18:41:00 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -0,0 +1,69 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    // 현재에서부터 가장 나중에 쓰일 녀석을 지금 플러그에서 빼자
+    static Set<Integer> set = new HashSet<>();
+    static int[] arr;
+    static int K;
+
+    static int find(int startIdx){
+        //set에서 하나씩 빼보면서 언제가 가장 끝에 있는지 + 아예 못찾으면 얘를 뺸다
+        int targetS = 0;
+        int targetIdx = -1; // 초기값을 -1로 설정
+        for(int s : set){
+            int findIdx = Integer.MAX_VALUE; // 초기값을 Integer.MAX_VALUE로 설정
+            for(int i = startIdx; i < K; i++){
+                if(s == arr[i]) {
+                    findIdx = i;
+                    break;
+                }
+            }
+            if(findIdx == Integer.MAX_VALUE) return s; // 찾지 못한 경우 해당 요소 반환
+            if(findIdx > targetIdx){
+                targetIdx = findIdx;
+                targetS = s;
+            }
+        }
+        return targetS;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        arr = new int[K];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < K; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int idx = 0;
+        for(int i = 0; i < K; i++){ // 처음에 플러그 꽂기
+            if(set.size() == N){
+                idx = i;
+                break;
+            } else {
+                set.add(arr[i]);
+            }
+        }
+
+        int cnt = 0;
+        for(int i = idx; i < K; i++){
+            // set에 있으면 넘어가고 없으면 하나 빼고 넣어야함
+            if(set.contains(arr[i])) continue; // set에 있으면 pass
+            else {
+                int toRemove = find(i+1); //set에 없으면 빼는 로직 실행
+                set.remove(toRemove);
+                cnt++;
+                set.add(arr[i]); // 뺐으면 새로 넣기
+            }
+        }
+        System.out.println(cnt);
+    }
+}
