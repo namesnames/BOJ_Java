62a726985c773373e56f30ea2035e1cca8c3f612
namesnames <103047410+namesnames@users.noreply.github.com>
2024-06-11 03:37:47 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 656 ms, Memory: 124960 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,6 +1,18 @@
 import java.io.*;
 import java.util.*;
 
+// https://www.youtube.com/watch?v=voklbG1wU8A 좋은 해설 영상
+// 하던대로 dp로 하면 시간초과
+// 아래 예시로 간단하게 설명하자면
+// 10 20 10 30 40 10 12 14 16 18
+// 일단 arraylist에 하나씩 넣어갈 건데 넣을 녀석이 list의 맨 마지막보다 크면 add
+// 작으면 list의 요소들 중에 대체가 가능한지 check
+// list = {10,20,30,40} 이 있을 때 12를 넣겠다하면
+// 요소들 중에 12<=x<40을 만족하고 그들 중 맨 왼쪽에 있는 거랑 바꿔치기함 (list.set이용)
+// 결국 list = {10,20,30,40} 에서
+// list = {10,12,14,16,18}이 되는건데 대체해도 이 list의 전체길이는 바뀌지 않으니까
+// for문 다 돌고 list의 길이를 출력하면 정답임
+
 public class Main {
     static int[] arr;
     static int[] dp;
@@ -8,7 +20,7 @@ public class Main {
 
     static int search(int start, int end, int target){
 
-        int result = list.size()-1;
+        int result = list.size()-1;  //이건 이제 list = {10,12,14,40}인 상황에서 16을 넣으려고 하는 상황을 위함 // 16<=x<40을 못찾으면 그냥 list맨 끝인 40이랑 바꿔쳐야 하니까 
         while(start <= end){
             int mid = (start + end) / 2;
             if(list.get(mid) > target){
@@ -43,9 +55,6 @@ public class Main {
             else list.set(search(0,list.size()-2, arr[i]),arr[i]);
         }
 
-//        for(int l : list){
-//            System.out.print(l + " ");
-//        }
         System.out.println(list.size());
     }
 }
