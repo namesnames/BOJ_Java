f81f3c1d19bbcaec3f3d1e80e95cf167fa136f8e
namesnames <103047410+namesnames@users.noreply.github.com>
2024-06-17 18:22:57 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 160 ms, Memory: 16360 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        boolean[] check = new boolean[100001];
+        int[] V = new int[100001];
+
+        int start = Integer.parseInt(st.nextToken());
+        int dest = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.add(start);
+        V[start] = 0;
+
+        while(!dq.isEmpty()){
+            int i = dq.poll();
+            if(i == dest) break;
+
+            int ni = i*2;
+            if(0<=ni && ni<=100000 && check[ni] == false){
+                V[ni] = V[i];
+                check[ni] = true;
+                dq.addFirst(ni);
+            }
+
+            ni = i-1;
+            if(0<=ni && ni<=100000 && check[ni] == false) {
+                V[ni] = V[i] + 1;
+                check[ni] = true;
+                dq.add(ni);
+            }
+
+            ni = i+1;
+            if(0<=ni && ni<=100000 && check[ni] == false) {
+                V[ni] = V[i] + 1;
+                check[ni] = true;
+                dq.add(ni);
+            }
+
+
+
+        }
+
+        System.out.println(V[dest]);
+    }
+}
