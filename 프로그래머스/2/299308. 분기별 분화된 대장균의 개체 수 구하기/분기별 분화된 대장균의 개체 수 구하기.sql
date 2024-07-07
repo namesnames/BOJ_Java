384e551ffec53b02bfe236f8dd6c9d6c1cffd915
namesnames <103047410+namesnames@users.noreply.github.com>
2024-07-07 22:07:27 +0900

    [level 2] Title: 분기별 분화된 대장균의 개체 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/299308. 분기별 분화된 대장균의 개체 수 구하기/분기별 분화된 대장균의 개체 수 구하기.sql
+++ 프로그래머스/2/299308. 분기별 분화된 대장균의 개체 수 구하기/분기별 분화된 대장균의 개체 수 구하기.sql
@@ -1,10 +1,19 @@
-select 
-    case when date_format(differentiation_date, "%m") in ("01","02","03") then '1Q'
-         when date_format(differentiation_date, "%m") in ("04","05","06") then '2Q'
-         when date_format(differentiation_date, "%m") in ("07","08","09") then '3Q'
-         when date_format(differentiation_date, "%m") in ("10","11","12") then '4Q'
-    end as quarter
-    , count(*) as ecoli_count
-    from ecoli_data
-    group by quarter
-    order by quarter
+# select 
+#     case when date_format(differentiation_date, "%m") in ("01","02","03") then '1Q'
+#          when date_format(differentiation_date, "%m") in ("04","05","06") then '2Q'
+#          when date_format(differentiation_date, "%m") in ("07","08","09") then '3Q'
+#          when date_format(differentiation_date, "%m") in ("10","11","12") then '4Q'
+#     end as quarter
+#     , count(*) as ecoli_count
+#     from ecoli_data
+#     group by quarter
+#     order by quarter
+
+select case when month(differentiation_date) in (1,2,3) then '1Q'
+            when month(differentiation_date) in (4,5,6) then '2Q'
+            when month(differentiation_date) in (7,8,9) then '3Q'
+            when month(differentiation_date) in (10,11,12) then '4Q'
+            end as quarter, count(*) as ecoli_count
+from ecoli_data
+group by quarter
+order by quarter
