b960ce2eafc04c07c7c63b3257b35f44af71e707
namesnames <103047410+namesnames@users.noreply.github.com>
2024-07-21 18:58:20 +0900

    [Gold II] Title: 나누기, Time: 300 ms, Memory: 24812 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.java
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -1,55 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        int n = Integer.parseInt(br.readLine());
+        long[] sum = new long[n];
+        long[] cnt = new long[4];
+        cnt[0] = 1;
+        
+        long s = 0;
+        long division;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long sum = 0;
-        for(int i=0; i<N; i++){
-            int a = Integer.parseInt(st.nextToken());
-            arr[i] = a;
-            sum += a;
+        for (int i = 0; i < n; i++) {
+            long input = Long.parseLong(st.nextToken());
+            s += input;
+            sum[i] = s;
         }
 
-        if(sum % 4 != 0){
+        if (sum[n - 1] % 4 != 0) {
             System.out.println(0);
-            return;
+        } else {
+            division = sum[n - 1] / 4;
+            for (int i = 0; i < n - 1; i++) {
+                if (sum[i] == 3 * division) {
+                    cnt[3] += cnt[2];
+                }
+                if (sum[i] == 2 * division) {
+                    cnt[2] += cnt[1];
+                }
+                if (sum[i] == 1 * division) {
+                    cnt[1] += cnt[0];
+                }
+            }
+            System.out.println(cnt[3]);
         }
-
-        int[] prefixSum = new int[N];
-        prefixSum[0] = arr[0];
-        for(int i=1; i<N; i++){
-            prefixSum[i] = prefixSum[i-1] + arr[i];
-        }
-
-        int[][] dp = new int[3][N];
-        if(prefixSum[0] == sum / 4) {
-            dp[0][0] = 1;
-        }
-
-        for(int i=1; i<N; i++){
-            if(prefixSum[i] == (sum/4) * 1) dp[0][i] = dp[0][i-1] + 1;
-            else dp[0][i] = dp[0][i-1];
-        }
-
-        dp[1][0] = 0;
-        for(int i=1; i<N; i++){
-            if(prefixSum[i] == (sum/4) * 2) dp[1][i] = dp[1][i-1] + dp[0][i];
-            else dp[1][i] = dp[1][i-1];
-        }
-
-        dp[2][0] = 0;
-        for(int i=1; i<N; i++){
-            if(prefixSum[i] == (sum/4) * 3) dp[2][i] = dp[2][i-1] + dp[1][i];
-            else dp[2][i] = dp[2][i-1];
-        }
-
-        System.out.println(dp[2][N-1]);
     }
 }
