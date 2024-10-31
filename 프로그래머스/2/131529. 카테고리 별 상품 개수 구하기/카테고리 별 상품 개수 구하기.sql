bd3f8591d9cc707d06d72fa76e6c8586e414833b
namesnames <103047410+namesnames@users.noreply.github.com>
2024-10-31 20:56:02 +0900

    [level 2] Title: 카테고리 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
@@ -1,4 +1,7 @@
-select substring(product_code,1,2) as category, count(*) as products
-from product 
+with sub as (select product_id, substring(product_code,1,2) as code
+             from product)
+             
+select sub.code as category, count(sub.product_id) as products
+from sub
 group by category
 order by category
